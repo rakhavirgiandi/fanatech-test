deb9af07b55766af19872095b48fb72b1dc69380
Rakha Ridwan Virgiandi <rakhardwan@gmail.com>
2024-12-21 00:01:34 +0700

    login page remake

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+        <div {{ $attributes->merge(['class' => 'form-text text-danger']) }}>{{ $message }}</div>
         @endforeach
-    </ul>
 @endif
